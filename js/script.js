b72d2d951eeb968a441e07c8c599fc7a4432ef07
May_Edu <mayck22.03@hotmail.com>
2023-09-22 11:24:22 -0300

    animações e novas funcionalidades

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-let opcao = 1
+// let opcao = 1
 
 //função do POPUP
-function abrir(){
+function abrir() {
     document.getElementById('popup').style.display = 'block';
 }
-function fechar(){
-    document.getElementById('popup').style.display =  'none';
+function fechar() {
+    document.getElementById('popup').style.display = 'none';
 }
 
+function reset(){
+    document.getElementById('opcao-voo-2').style.display = 'none';
+    document.getElementById('opcao-voo-1').style.display = 'none';
+}
 //função do idas e voltas
-function trocar(){
-    if(opcao = 1){
-        document.getElementById('opcao-voo-1').style.display = 'flex';
-        document.getElementById('opcao-voo-2').style.display = 'none';
-    }
+function trocar() {
+    document.getElementById('opcao-voo-1').style.display = 'flex';
+    document.getElementById('opcao-voo-2').style.display = 'none';
 }
 
-function trocar_dnv(){
-    if(opcao = 1){
-        document.getElementById('opcao-voo-1').style.display = 'none';
-        document.getElementById('opcao-voo-2').style.display = 'flex';
-    }
+function trocar_dnv() {
+    document.getElementById('opcao-voo-1').style.display = 'none';
+    document.getElementById('opcao-voo-2').style.display = 'flex';
 }
 
 
