38fa5b7b49dbaa6f9c4c121c89b79eadaff61f75
May_Edu <mayck22.03@hotmail.com>
2023-09-22 13:51:58 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
+setTimeout(abrir, 3000);
+setTimeout(limpar);
 // let opcao = 1
 
 //função do POPUP
 function abrir() {
+
     document.getElementById('popup').style.display = 'block';
 }
 function fechar() {
     document.getElementById('popup').style.display = 'none';
 }
 
-function reset(){
+function limpar() {
     document.getElementById('opcao-voo-2').style.display = 'none';
-    document.getElementById('opcao-voo-1').style.display = 'none';
+    // document.getElementById('opcao-voo-1').style.display = 'none';
+    
 }
 //função do idas e voltas
 function trocar() {
